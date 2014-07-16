06efd8e8b035fe7d7d5f5bc8b7896c41d0b9cf8f
Ville Vanninen <ville@workflower.fi>
2014-07-17 00:09:56 +0300

    parse vocab syntax samples with haml

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,19 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    haml: {
+      vocabs: {
+        //TODO: how to remove all whitespace while compiling?
+        //options {remove_whitespace: true, ugly: true} didn't work
+        files: [{
+          expand: true,
+          cwd: 'app/vocabs/',
+          src: ['*/sample.haml'],
+          dest: 'app/vocabs/',
+          ext: '.html'
+        }],
+      }
+    },
     sass: {
       app: {
         files: {
@@ -23,7 +36,15 @@ module.exports = function(grunt) {
         files: {
           'app/assets/css/happy.css': 'app/assets/css/happy.css'
         }
-        //TODO: autoprefix vocabs too
+      },
+      vocabs: {
+        files: [{
+          expand: true,
+          cwd: 'app/vocabs/',
+          src: ['*/syntax.css'],
+          dest: 'app/vocabs/',
+          ext: '.css'
+        }],
       }
     },
     devserver: {
@@ -36,9 +57,9 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      reload: {
+      appfiles: {
         files: [
-          'app/assets/*.html',
+          'app/*.html',
           'app/assets/css/*.css',
           'app/assets/js/*.js'
         ],
@@ -47,12 +68,22 @@ module.exports = function(grunt) {
           interrupt: true,
         }
       },
-      sass: {
+      appstyles: {
         files: [
           'app/assets/scss/*.scss',
-          'app/vocabs/*/syntax.scss'
         ],
-        tasks: ['sass', 'autoprefixer'],
+        tasks: ['sass:app', 'autoprefixer:app'],
+        options: {
+          livereload: false,
+          interrupt: true,
+        }
+      },
+      vocabs: {
+        files: [
+          'app/vocabs/*/syntax.scss',
+          'app/vocabs/*/sample.haml',
+        ],
+        tasks: ['haml:vocabs', 'sass:vocabs', 'autoprefixer:vocabs'],
         options: {
           livereload: false,
           interrupt: true,
@@ -63,12 +94,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-haml');
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-devserver');
 
   grunt.registerTask('serve', ['devserver']);
-  grunt.registerTask('app', ['devserver:app']);
-  grunt.registerTask('site', ['devserver:site']);
   grunt.registerTask('default', ['watch']);
 
 };
