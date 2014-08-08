0f81fc3b0729f18b858cd740b1be7ac510dc4f3d
Ville Vanninen <ville@workflower.fi>
2014-08-09 02:14:55 +0300

    added analytics

--- app/app.php
+++ app/app.php
@@ -62,7 +62,6 @@
         <br>
         <?php echo $appCredits ?>
         <br>
-        <?php // echoVocabsLinkList($vocabs); ?>
       </div>
     </div>
     <div class="vocab-sidebar">
@@ -84,5 +83,15 @@
       <code class="vocab-code"><?php readfile('vocabs/'.$vocabName.'.vocab/sample.html'); ?></code>
     </div>
   </div>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-21435402-3', 'auto');
+    ga('send', 'pageview');
+  </script>
+
 </body>
 </html>
